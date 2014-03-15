adb581049c70bbc720f923206eafa0d24ab77149
Nahuel Chaves <nahuel.chaves@gmail.com>
2014-03-15 01:05:28 -0300

    scheduler.js production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Turnos::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( dashboard.js dashboard.css )
+  config.assets.precompile += %w( dashboard.js dashboard.css dashboard/scheduler.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
